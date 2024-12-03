54ead347a46499f3e8dd49a2c381de2995fc0a18
rkdrjsrks <rkdrjs71@naver.com>
2024-12-04 08:04:58 +0900

    feat: 카메라 구현

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        domains: ['15.165.25.180'],
+    },
 };
 
 export default nextConfig;
